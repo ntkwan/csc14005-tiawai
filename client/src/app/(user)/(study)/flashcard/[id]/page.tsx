f0aca1e51072a79161426cd6b8db2c34cf7b4535
Nguyen Trung Quan <22127346@student.hcmus.edu.vn>
2024-10-31 00:48:26 +0700

    [TIA-65/TIA-64] feat: add flashcard page (#14)
    * [TIA-69][TIA-68] feat: add about us page
    
    * [TIA-59][TIA-46] feat: add sign in and sign up page
    
    * [TIA-59][TIA-46] refactor: change importing ui route in tsconfig
    
    * [TIA-65][TIA-64] feat: add flashcard page
    
    * [TIA-65/TIA-64] feat: delete index parameter in Flashcard component
    
    * [TIA-65/TIA-64] refactor: change flashcard slider props type
    
    ---------
    
    Co-authored-by: Npeka <npkhang22@clc.fitus.edu.vn>

---
+++ client/src/app/(user)/(study)/flashcard/[id]/page.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import Link from "next/link";
+import FlashcardSlider from "./flashcard-slider";
+import { CarryOutOutlined, LeftOutlined } from "@ant-design/icons";
+import { useParams } from "next/navigation";
+import { Row, Col } from "antd";
+
+import {
+    flashcardsScience,
+    flashcardsResearch,
+    flashcardsOffice,
+    flashcardsIT,
+    flashcardsLiterature,
+    flashcardsCulture,
+} from "./flashcards";
+
+export default function FlashCardItemPage() {
+    const { id } = useParams();
+    const decodedId = id
+        ? decodeURIComponent(Array.isArray(id) ? id[0] : id)
+        : "Unknown";
+
+    const data = [
+        {
+            topic: "Khoa học",
+            words: flashcardsScience,
+        },
+        {
+            topic: "Nghiên cứu",
+            words: flashcardsResearch,
+        },
+        {
+            topic: "Văn phòng",
+            words: flashcardsOffice,
+        },
+        {
+            topic: "IT",
+            words: flashcardsIT,
+        },
+        {
+            topic: "Văn học",
+            words: flashcardsLiterature,
+        },
+        {
+            topic: "Văn hóa",
+            words: flashcardsCulture,
+        },
+    ];
+
+    const flashcards = data.find((item) => item.topic === decodedId)?.words;
+
+    return (
+        <div className="bg-[url('/home-icon-bg.svg')]">
+            <Row gutter={[0, 40]}>
+                <Col offset={2}>
+                    <Link
+                        className="flex items-center gap-2 rounded-full bg-[#e9dae9] px-4 py-3 text-black"
+                        href="/flashcard"
+                    >
+                        <LeftOutlined />
+                        <span className="text-xl font-bold">
+                            Flashcard {decodedId}
+                        </span>
+                        <div className="rounded-full bg-[#f4edf4] px-4 py-1">
+                            <CarryOutOutlined className="text-[#4d2c5e]" />
+                        </div>
+                    </Link>
+                </Col>
+
+                <Col span={20} offset={2}>
+                    <FlashcardSlider flashcards={flashcards || []} />
+                </Col>
+            </Row>
+        </div>
+    );
+}
