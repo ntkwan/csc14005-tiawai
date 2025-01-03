17cc17a735754f834d722d53b15c75636845ef04
Npeka <npkhang22@clc.fitus.edu.vn>
2025-01-03 18:40:25 +0700

    refactor: change "Tiawai" to "tiawai"

--- client/src/ui/tiawai-mascot.tsx
+++ client/src/ui/tiawai-mascot.tsx
@@ -1,9 +1,9 @@
-'use client';
-import Image from 'next/image';
-import { Typography } from 'antd';
-import mascotCap from '@public/mascot/cap.png';
-import mascotBody from '@public/mascot/body.png';
-import mascotBgBlur from '@public/mascot/bg-blur.png';
+"use client";
+import Image from "next/image";
+import { Typography } from "antd";
+import mascotCap from "@public/mascot/cap.png";
+import mascotBody from "@public/mascot/body.png";
+import mascotBgBlur from "@public/mascot/bg-blur.png";
 const { Title } = Typography;
 
 export default function TiawaiMascot() {
@@ -31,7 +31,7 @@ export default function TiawaiMascot() {
                     className="tiawai-mascot__name text-center !font-chango !font-normal"
                     level={2}
                 >
-                    Tiawai
+                    tiawai
                 </Title>
             </div>
         </div>
